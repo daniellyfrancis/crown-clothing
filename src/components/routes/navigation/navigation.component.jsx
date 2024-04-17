e3bd30db3aff9bfc96480c8ac43da08502dc2019
danyfrancisdev <francisdanielly04@gmail.com>
2024-04-17 18:17:18 +0100

    navigation updates

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -9,12 +9,8 @@ import { UserContext } from '../../../contexts/user.context';
 import {signOutUser} from '../../../utils/firebase/firebase.utils';
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { currentUser } = useContext(UserContext);
 
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  }
   return (
     <Fragment>
       <div className='navigation'>
@@ -26,7 +22,10 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-              <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>
+              <span className="nav-link" onClick={signOutUser}>
+                {' '}
+                SIGN OUT{' '}
+              </span>
           ) : (
                 <Link className='nav-link' to='/auth'>
                   SIGN IN
