5978c9da469bce66ff5dba6b20a65d9887964ad9
danyfrancisdev <francisdanielly04@gmail.com>
2024-04-17 16:58:51 +0100

    sign in updates

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+
+import { UserContext } from "../../contexts/user.context";
 
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 
@@ -16,7 +18,7 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
-  console.log(formFields);
+  const { setCurrentUser } = useContext(UserContext); 
 
 const resetFormFields = () => {
   setFormFields(defaultFormFields);
@@ -31,9 +33,8 @@ const handleSubmit = async (event) => {
   event.preventDefault();
 
   try {
-    const response = await signInAuthUserWithEmailAndPassword(email, password);
-    console.log(response);
-    resetFormFields();
+    const { user } = await signInAuthUserWithEmailAndPassword(email, password);
+    setCurrentUser(user);
 
   } catch(error) {
     switch(error.code) {
