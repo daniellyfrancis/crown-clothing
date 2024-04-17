97b9caaee95c661bcdabc47fc1a51be67e8e50c5
danyfrancisdev <francisdanielly04@gmail.com>
2024-04-17 09:14:30 +0100

    lats updates

--- src/components/routes/authentication/authentication.component.jsx
+++ src/components/routes/authentication/authentication.component.jsx
@@ -1,7 +1,7 @@
 import { createUserDocumentFromAuth, signInWithGooglePopup } from "../../../utils/firebase/firebase.utils";
 import SignUpForm from "../../sign-up-form/sign-up-form.component";
 
-const SignIn = () => {
+const Authentication = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -17,4 +17,4 @@ const SignIn = () => {
   )
 }
 
-export default SignIn;
+export default Authentication;
